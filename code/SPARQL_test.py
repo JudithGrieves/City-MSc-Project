81acd769f277ca2a23723a8cf8d696390f772979
Judith Grieves <judith_grieves@hotmail.com>
2021-07-12 15:18:00 +0100

    change to parse both KG files, metrics (psp) and scanner(scn), update columns to show all data.

--- code/SPARQL_test.py
+++ code/SPARQL_test.py
@@ -33,38 +33,47 @@ def query_all(g,outfile):
     Query to return all the CSV columns that were converted to RDF
     To be used as part of a test plan
     ''' 
+    # ?fsunit not in original TabularData sheet but included here to show 
+    # that the scanner data is being picked up from a separate KG
     qres = g.query(
-    """SELECT DISTINCT  ?patient_label   ?visit_label  ?sex ?age ?bmi ?liver_cT1   ?liver_PDFF ?liver_T2Star ?scanner_label ?fieldsstr  WHERE
+    """SELECT DISTINCT  ?patient_label   ?visit_label ?age  ?sex ?bmi  \
+                        ?liver_cT1   ?liver_PDFF ?liver_T2Star ?scanner_label \
+                        ?fs ?fsunit ?manf_label WHERE
                 {
-                 ?visit a ?ScanVisit .
+                 ?visit a psp:ScanVisit .
                  ?visit rdfs:label ?visit_label .
                  ?visit psp:isAttendedBy ?patient .  
-                 ?patient a ?Patient .
+                 ?patient a psp:Patient .
                  ?patient rdfs:label ?patient_label .
                  ?patient psp:PatientSex ?sex .  
                  ?patient psp:PatientAge ?age . 
                  ?patient psp:PatientBMI ?bmi . 
                  ?scanner psp:usedInVisit ?visit .
                  ?scanner rdfs:label ?scanner_label .
-                 ?scanner psp:FieldStrength ?fieldsstr
+                 ?scanner a scn:MRIScannerModel .
+                 ?scanner scn:FieldStrength ?fs .
+                 ?scanner scn:FieldStrengthUnit  ?fsunit .
+                 ?manf a scn:ScannerManufacturer .
+                 ?manf rdfs:label ?manf_label .
+                 ?manf scn:isMakerOf ?scanner.
+                 
                  {?metric1 psp:isMetricForPatient  ?patient  .
                  ?metric1 psp:MetricValue ?liver_cT1 .
-                 ?metric1 a <http://www.perspectum.com/resources/liver_cT1> .}
+                 ?metric1 a psp:liver_cT1  .}
                  UNION
                  {?metric2 psp:isMetricForPatient  ?patient  .
                  ?metric2 psp:MetricValue ?liver_PDFF .
-                 ?metric2 a <http://www.perspectum.com/resources/liver_PDFF> .}
+                 ?metric2 a psp:liver_PDFF .}
                  UNION
                  {?metric3 psp:isMetricForPatient  ?patient  .
                  ?metric3 psp:MetricValue ?liver_T2Star .
-                 ?metric3 a <http://www.perspectum.com/resources/liver_T2Star> .}
+                 ?metric3 a psp:liver_T2Star .}
 } GROUP BY ?patient_label 
-    ORDER BY ASC(?patient_label) """) 
-    
-    #    FILTER regex(?metric_type, "Named", "i" )
+    ORDER BY ASC(?visit_label) ASC(?patient_label) """) 
 
     print("Show all Data")   
-    header='"PatientID","Visit","Age","Sex","BMI","liver_cT1","liver_PDFF","liver_T2Star","Scanner","Field Strength"'  
+    header='"PatientID","Visit","Age","Sex","BMI","liver_cT1","liver_PDFF",\
+            "liver_T2Star","Scanner","Field Strength","Unit","Manufacturer"'  
     write_sparql(outfile,header,qres,1,1)  
     
 def main():
@@ -75,17 +84,21 @@ def main():
     output_dir = data_dir
         
         
-    inFile="test_data14.ttl" 
-    outFile=inFile.replace(".ttl", "-")+"replicate.csv"  
+    inFile1="metrics_data.ttl" 
+    inFile2="scanner_data.ttl" 
+    outFile=inFile1.replace(".ttl", "-")+"replicate.csv"  
     
-    inFile = os.path.join(data_dir,inFile)
+    inFile1 = os.path.join(data_dir,inFile1)
+    inFile2 = os.path.join(data_dir,inFile2)
     outFile = os.path.join(output_dir,outFile)
     
 
     
-    print("Load a graph from input file: ", inFile)
+    print("Load a graph from input file: ", inFile1)
     g = Graph()    
-    g.parse(inFile, format="ttl")         
+    g.parse(inFile1, format="ttl")         
+    print("Loaded '" + str(len(g)) + "' triples.\n")  
+    g.parse(inFile2, format="ttl")         
     print("Loaded '" + str(len(g)) + "' triples.\n")    
 
     # run SPARQL queries against the loaded graph
